d1ec7eafd75732ed2d0b938cd53746c632eacbde
Dunvant_kai <77610897+Dunvantkai@users.noreply.github.com>
2025-02-02 22:57:09 +1300

    update

---
+++ Birthday Manager/Birthday Manager/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Birthday_Manager
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
